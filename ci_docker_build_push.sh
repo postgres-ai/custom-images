a4b5867d275f9758f8db6ab32ef50fad38aac29a
akartasov <agneum@gmail.com>
2022-11-22 16:43:27 +0700

    add base commit ref, build timescale cloud images

--- ci_docker_build_push.sh
+++ ci_docker_build_push.sh
@@ -6,6 +6,7 @@ docker_file=${DOCKER_FILE:-""}
 tags=${TAGS:-""}
 pg_server_version=${PG_SERVER_VERSION:-""}
 base_image=${BASE_IMAGE_NAME:-""}
+base_commit_ref=${BASE_COMMIT_REF:-""}
 
 registry_user=${REGISTRY_USER:-"${CI_REGISTRY_USER}"}
 registry_password=${REGISTRY_PASSWORD:-"${CI_REGISTRY_PASSWORD}"}
@@ -26,7 +27,8 @@ for tag in "${ADDR[@]}"; do
 done
 
 set -x
-docker build --build-arg PG_SERVER_VERSION=$pg_server_version --build-arg BASE_IMAGE_NAME=$base_image $tags_build --file $docker_file .
+docker build --build-arg BASE_IMAGE_NAME=$base_image --build-arg PG_SERVER_VERSION=$pg_server_version \
+  --build-arg BASE_COMMIT_REF=$base_commit_ref $tags_build --file $docker_file .
 set +x
 
 echo -e "$tags_push" | while read -r tag; do
