8b328b396a4961f774954426c33f3abe039d0af2
Vitaliy Kukharik <vitaliy@postgres.ai>
2022-11-25 21:11:53 +0300

    add pgrouting extension to postgis image

--- extended-postgres/postgis.sh
+++ extended-postgres/postgis.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 # postgis extension
+# (+ address_standardizer, postgis_raster, postgis_sfcgal, postgis_tiger_geocoder, postgis_topology)
 apt-get install -y --no-install-recommends \
    postgresql-${PG_SERVER_VERSION}-postgis-${POSTGIS_VERSION} \
    postgresql-${PG_SERVER_VERSION}-postgis-${POSTGIS_VERSION}-scripts
+
+# pgrouting extension
+apt-get install -y --no-install-recommends \
+   postgresql-${PG_SERVER_VERSION}-pgrouting
