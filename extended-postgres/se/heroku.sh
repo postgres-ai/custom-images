30f986209aa55a69370d9f7ae1c3b7526b9f8fd9
Vitaliy Kukharik <vitaliy@postgres.ai>
2022-12-05 20:15:47 +0300

    add build-env and heroku-libs.sh

--- extended-postgres/se/heroku.sh
+++ extended-postgres/se/heroku.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-apt-get update -o Acquire::CompressionTypes::Order::=gz
-apt-get install --no-install-recommends -y apt-transport-https ca-certificates \
-   wget curl sudo git make cmake gcc build-essential bc unzip apt-utils lsb-release \
-   libreadline-dev zlib1g-dev flex bison libxml2-dev libxslt-dev libssl-dev libxml2-utils \
-   xsltproc ccache libbrotli-dev liblzo2-dev libsodium-dev libc6-dev krb5-multidev libkrb5-dev \
-   postgresql-server-dev-${PG_SERVER_VERSION} libpq-dev libcurl4-openssl-dev python2 \
-   python3 pkg-config clang g++ libc++-dev libc++abi-dev libglib2.0-dev libtinfo5 ninja-build binutils libicu-dev
+set -ex
+
+apt-get clean && rm -rf /var/lib/apt/lists/partial \
+&& apt-get update -o Acquire::CompressionTypes::Order::=gz \
+&& apt-get install --no-install-recommends -y apt-transport-https ca-certificates \
+  bc lsb-release apt-utils
 
 # postgis extension
 # (+ address_standardizer, postgis_raster, postgis_sfcgal, postgis_tiger_geocoder, postgis_topology)
@@ -22,12 +21,10 @@ apt-get install -y --no-install-recommends \
 # already in the "Generic" image
 
 # pgauditlogtofile extension
-cd /tmp && wget --quiet -O /tmp/pgauditlogtofile-${PGAUDITLOGTOFILE_VERSION}.tar.gz \
-  https://github.com/fmbiete/pgauditlogtofile/archive/refs/tags/v${PGAUDITLOGTOFILE_VERSION}.tar.gz \
-  && tar zxf /tmp/pgauditlogtofile-${PGAUDITLOGTOFILE_VERSION}.tar.gz \
-  && cd /tmp/pgauditlogtofile-${PGAUDITLOGTOFILE_VERSION} && make USE_PGXS=1 && make USE_PGXS=1 install
+# copy from build-env
 
 # remove all auxiliary packages to reduce final image size
-cd / && rm -rf /tmp/* && apt-get purge -y --auto-remove wget curl apt-transport-https apt-utils lsb-release bc \
-&& apt-get clean -y autoclean \
-&& rm -rf /var/lib/apt/lists/*
+apt-get purge -y --auto-remove \
+  apt-transport-https apt-utils lsb-release bc
+apt-get clean -y autoclean
+rm -rf /var/lib/apt/lists/*
