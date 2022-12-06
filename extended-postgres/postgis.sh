48547386477596077da36f3306a5cd3765d6ccb5
Vitaliy Kukharik <vitaliy@postgres.ai>
2022-12-06 21:09:29 +0300

    update postgis.sh (add apt-get update)

--- extended-postgres/postgis.sh
+++ extended-postgres/postgis.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# remove the "beta" and "rc" suffix in the PG_SERVER_VERSION variable (if exists)
+PG_SERVER_VERSION="$( echo ${PG_SERVER_VERSION} | sed 's/beta.*//' | sed 's/rc.*//' )"
+
+apt-get clean && rm -rf /var/lib/apt/lists/partial \
+&& apt-get update -o Acquire::CompressionTypes::Order::=gz \
+&& apt-get install --no-install-recommends -y \
+   apt-transport-https ca-certificates lsb-release apt-utils
+
 # postgis extension
 # (+ address_standardizer, postgis_raster, postgis_sfcgal, postgis_tiger_geocoder, postgis_topology)
 apt-get install -y --no-install-recommends \
@@ -9,3 +17,9 @@ apt-get install -y --no-install-recommends \
 # pgrouting extension
 apt-get install -y --no-install-recommends \
    postgresql-${PG_SERVER_VERSION}-pgrouting
+
+# remove all auxiliary packages to reduce final image size
+apt-get purge -y --auto-remove \
+  apt-transport-https ca-certificates lsb-release apt-utils
+apt-get clean -y autoclean
+rm -rf /var/lib/apt/lists/*
